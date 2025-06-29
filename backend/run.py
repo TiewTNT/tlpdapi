1a8f7b3574b667b5b5ffa2ff6772a586b911ab64
TiewTNT <105199283+TiewTNT@users.noreply.github.com>
2025-06-30 02:21:53 +1000

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,8 @@
 import subprocess
 from pathlib import Path
+import sys
 
 current_file = Path(__file__)
 current_dir = current_file.resolve().parent
 
-subprocess.run(['py', '-m', 'uvicorn', 'app:app', '--reload'], check=True, cwd=current_dir/'app')
+subprocess.run([Path(sys.executable), '-m', 'uvicorn', 'app:app', '--reload'], check=True, cwd=current_dir/'app')
