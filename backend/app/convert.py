45257d13dfd1e78b893890b6a510d5b9492401cb
Tiew <tnttiew@gmail.com>
2025-07-02 23:33:20 +0700

    Refactor: Comment out unused function and related code in convert.py

--- backend/app/convert.py
+++ backend/app/convert.py
@@ -5,15 +5,15 @@ import os
 import regex
 
 
-def remove_patterns_from_file(file_path: Path, patterns: regex.Pattern):
-    """Remove lines matching the given regex patterns from a file."""
-    with file_path.open('r', encoding='utf-8') as file:
-        html = file.read()
+# def remove_patterns_from_file(file_path: Path, patterns: regex.Pattern):
+#     """Remove lines matching the given regex patterns from a file."""
+#     with file_path.open('r', encoding='utf-8') as file:
+#         html = file.read()
 
-    with file_path.open('w', encoding='utf-8') as file:
-        file.write(
-            regex.sub(patterns, '', html)
-        )
+#     with file_path.open('w', encoding='utf-8') as file:
+#         file.write(
+#             regex.sub(patterns, '', html)
+#         )
 
 
 def convert(file_paths, output_folder, zip_folder, format='pdf', image_format='png', dpi=200):
@@ -27,13 +27,13 @@ def convert(file_paths, output_folder, zip_folder, format='pdf', image_format='p
             if format in ['md', 'txt']:
                 subprocess.run(['pdftohtml', '-c', '-s', '-noframes', str(file_path),
                             Path(str(output_folder / file_path.stem) + '.html')], check=True)
-                remove_patterns_from_file(Path(str(output_folder / file_path.stem) + '.html'),
-                                        regex.compile(rf'<img\s+width="\d+"\s+height="\d+"\s+src="{regex.escape(file_path.stem)}\d+\.png"\s+alt="background image"\s*/?>', regex.DOTALL))
+                # remove_patterns_from_file(Path(str(output_folder / file_path.stem) + '.html'),
+                #                         regex.compile(rf'<img\s+width="\d+"\s+height="\d+"\s+src="{regex.escape(file_path.stem)}\d+\.png"\s+alt="background image"\s*/?>', regex.DOTALL))
                 
-                subprocess.run(['pandoc', Path(str(output_folder / file_path.stem) + '.html'), '-t', 'markdown_strict',
-                            '-o', Path(str(output_folder / file_path.stem) + '.md')], check=True)
+                # subprocess.run(['pandoc', Path(str(output_folder / file_path.stem) + '.html'), '-t', 'markdown_strict',
+                #             '-o', Path(str(output_folder / file_path.stem) + '.md')], check=True)
                 
-                os.remove(Path(str(output_folder / file_path.stem) + '001.png'))
+                # os.remove(Path(str(output_folder / file_path.stem) + '001.png'))
                 os.remove(Path(str(output_folder / file_path.stem) + '.html'))
                 if format == 'txt':
                     subprocess.run(['pandoc', Path(str(output_folder / file_path.stem) + '.md'), '-t', 'plain',
